69d8bc8222e6ce657ee2ed54c3f43d2ce07e8c9f
MarceloBertochi <paidasamia@gmail.com>
2023-05-01 02:07:38 -0300

    Exercicio da Etec - Diabete Gestacional

---
+++ DiabetesGestacional/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DiabetesGestacional
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            int
+            valorGlicemia;
+
+
+            Console.Clear();
+
+            Console.WriteLine("--- Diabetes Gestacional ---\n");
+
+            Console.Write("Digite o valor de glicemia (em mg/dL)..: ");
+            valorGlicemia = Convert.ToInt32(Console.ReadLine());
+
+            if (valorGlicemia < 92)
+
+            {
+                Console.WriteLine("Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose\n");
+            }
+
+            else if (valorGlicemia >= 92 && valorGlicemia < 126)
+            {
+                Console.WriteLine("Compatível com Diabetes Gestacional\n");
+            }
+
+            else if (valorGlicemia >= 126)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Beep(); Console.Beep(); Console.Beep();
+                Console.WriteLine("Diabetes Mellitus na Gravidez\n");
+                Console.ResetColor();
+            }
+        }
+    }
+}
+
