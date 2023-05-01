584f2097acc6aa232131528daf5dfe7f9d22a783
MarceloBertochi <paidasamia@gmail.com>
2023-05-01 00:50:16 -0300

    Exercicio da Etec - Rolagem de Dados

---
+++ rolagemDeDado/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace RolagemDeDados
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Clear();
+            Console.WriteLine("-- Rolagem de dados --");
+            Console.WriteLine();
+
+            Console.Write("Quantas faces tem o dado? ..: ");
+            int faces = Convert.ToInt32(Console.ReadLine());
+
+            int rolagem = RandomNumberGenerator.GetInt32(1, faces);
+
+            Console.WriteLine($"Rolagem: {rolagem}");
+
+        }
+    }
+}
