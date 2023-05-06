4421eafc1d749948638b49c315cb064af87694fd
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-05 22:33:29 -0300

    Exercicio para nota Etec Luz de advertencia

--- LuzDeAdvertencia/Program.cs
+++ LuzDeAdvertencia/Program.cs
@@ -11,12 +11,57 @@ namespace LuzDeAdvertencia
         {
             Console.Clear();
 
-            Console.WriteLine("--- Luz De Advertência ---");
+            Console.WriteLine("--- Luz De Advertência ---\n");
 
-            Boolean 
-            t = true,
-            p = true,
-            r = false;
+            string 
+            t, // Temperatura
+            p, // Pressão
+            r; // Rotação
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("--- Simulador de Luz de Advertência ---\n");
+            Console.ResetColor();
+
+            Console.WriteLine("[A]tivado, [D]esativado");
+
+            Console.Write("Sensor de temperatura...:");
+            t = Console.ReadLine().ToUpper()!;
+
+            Console.Write("Sensor de Pressão.......:");
+            p = Console.ReadLine().ToUpper()!;
+
+            Console.Write("Sensor de Rotação...:");
+            r = Console.ReadLine().ToUpper()!;
+
+            Console.WriteLine();
+
+            if (t != "A" || t != "D" || p != "A" || p != "D" || r != "A" || r != "D")
+            {
+                 Console.WriteLine("Digite Invalido\n");
+            }
+            else if (t != "A" && p != "A" && r != "D")
+            {
+                Console.WriteLine("Luz de advertência DESATIVADO.\n");
+            }
+            else if (t == "A" && p == "A")
+            {
+                Console.WriteLine("Luz de advertência ATIVADA.\n");
+            }
+            else if (t == "A" && r == "A")
+            {
+                Console.WriteLine("Luz de advertência ATIVADA.\n");
+            }
+
+
+
+
+
+
+
+
+
+
+
 
             
 
@@ -24,3 +69,6 @@ namespace LuzDeAdvertencia
         }
     }
 }
+
+
+//(t != "A" || t != "D" || p != "A" || )
--- LuzDeAdvertencia/README.md
+++ LuzDeAdvertencia/README.md
@@ -5,8 +5,9 @@ Exercício LuzDeAdvertencia
 Uma luz de advertência W é acessa no painel de um automóvel somente quando:
 
 o sensor T de temperatura está ativado, e...
-...pelo menos uma das duas situações seguintes ocorrem:
+
 o sensor P de pressão está ativado.
+
 o sensor R de rotação está desativado.
 
 
@@ -22,4 +23,3 @@ Sensor de temperatura...: A
 Sensor de pressão.......: D
 Sensor de rotação.......: D
 
-A luz de advertência estará ATIVADA.
