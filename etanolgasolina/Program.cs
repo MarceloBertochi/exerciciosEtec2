edad2b59cdaedbb173dc0f22319a3ee9555ba7c5
MarceloBertochi <paidasamia@gmail.com>
2023-05-18 16:04:07 -0300

    Exercicio em andamento

---
+++ etanolgasolina/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace EtanolGasolina
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            //etano, gasolina
+
+            LimparTela();
+            MinhaAssinatura();
+            ResetarCor();
+            PularLinha();
+
+            Console.WriteLine("Exercício Alcool Ou Gasolina");
+            LinhaAmarela();
+            PularLinha();
+
+            Console.WriteLine("--- Etanol ou Gasolina? ---");
+            PularLinha();
+
+            Console.Write("Digite o preço do etanol (R$).......:");
+            Console.ReadLine();
+            Console.Write("Digite o preço da gosolina (R$).....:");
+            Console.ReadLine();
+            PularLinha();
+
+            Console.WriteLine("O preço do etanol corresponde a ?? % do preço da gasolina.");
+            PularLinha();
+
+            Console.WriteLine("Recomendação: Abasteça com ETANOL.");
+        }
+
+
+
+
+
+
+        static void MinhaAssinatura()
+        {
+            LinhaAmarela();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Trabalho para Nota do Aluno ");
+            BarraAmarelaFinal();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Marcelo Camara Bertochi ");
+            BarraAmarelaFinal2();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Curso Informática I - Noite ");
+            BarraAmarelaFinal();
+            LinhaAmarela();
+        }
+
+        static void LinhaAmarela()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("-------------------------------\n");
+            Console.ResetColor();
+        }
+
+        static void BarraAmarelaInicio()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|");
+            Console.ResetColor();
+        }
+        static void BarraAmarelaFinal()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|\n");
+            Console.ResetColor();
+        }
+
+        static void BarraAmarelaFinal2()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|\n".PadLeft(6, ' '));
+            Console.ResetColor();
+        }
+        static void ResetarCor()
+        {
+            Console.ResetColor();
+        }
+
+        static void LimparTela()
+        {
+            Console.Clear();
+        }
+
+        static void PularLinha()
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+
+
+}
--- etanolgasolina/README.md
+++ etanolgasolina/README.md
@@ -1,2 +1,28 @@
 # exerciciosEtec2
 Exercicios da Etec
+
+Exercício AlcoolOuGasolina
+Um automóvel flex pode ser abastecido com gasolina ou com etanol (álcool). O senso comum diz que é mais vantajoso abastecer com gasolina caso a relação entre os preços seja maior do que 70%. Esta pesquisa diz que o percentual ideal deve ser próximo de 73%.
+
+Escreva uma sub-rotina que retorne a razão entre o preço do etanol e da gasolina (etanol ÷ gasolina). Escreva outra sub-rotina que utilize o resultado da primeira para retornar um booleano indicando se vale a pena ou não abastecer com gasolina (considere o percentual de 73%).
+
+Escreva um programa que recebe o valor do etanol e o valor da gasolina. Exiba a razão entre os preços e a recomendação de uso. Utilize as sub-rotinas criadas.
+
+Exemplos:
+
+--- Etanol ou Gasolina? ---
+
+Digite o preço do etanol (R$).....: 4,00
+Digite o preço da gasolina (R$)...: 6,00
+
+O preço do etanol corresponde a 66,7% do preço da gasolina.
+
+Recomendação: Abasteça com ETANOL.
+--- Etanol ou Gasolina? ---
+
+Digite o preço do etanol (R$).....: 5,20
+Digite o preço da gasolina (R$)...: 7,00
+
+O preço do etanol corresponde a 74,3% do preço da gasolina.
+
+Recomendação: Abasteça com GASOLINA.
