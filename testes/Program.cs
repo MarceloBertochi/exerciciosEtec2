4a3657aca354b9e88fabcad5d0a5a47049bbcd05
MarceloBertochi <paidasamia@gmail.com>
2023-05-28 18:12:06 -0300

    arquivo para terstar

---
+++ testes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgramaParaTeste
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            int x, y;
+
+            Console.Write("digite um número para ser somado..: ");
+            x = Int32.TryParse(Console.ReadLine()), out x;
+
+
+        }
+    }
+}
