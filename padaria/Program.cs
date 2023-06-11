268b965adbd2cb5dcb5eebd789a6b31d7f48306d
MarceloBertochi <paidasamia@gmail.com>
2023-06-11 18:11:38 -0300

    Metade pronto

---
+++ padaria/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Padaria
+
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+LimparTela();
+MinhaAssinatura();
+PularLinha();
+
+double
+grama,
+farinha,
+agua,
+fermento,
+sal;
+
+
+Console.WriteLine("--- Padaria ---");
+Console.Write("Peso desejado de pão italiano (em gramas)...: ");
+grama = Convert.ToDouble(Console.ReadLine());
+LinhaAmarela();
+
+Console.WriteLine("ingredientes :");
+Console.WriteLine($"Farinha {Convert.ToDecimal((farinha = grama)/(1+0.7+0.4+0.02))} gr");
+Console.WriteLine($"Água {Convert.ToDecimal( agua = (grama*70)/(1+0.7+0.4+0.02)/100)} gr");
+Console.WriteLine($"Fermento {fermento = (grama*40)/(1+0.7+0.4+0.02)/100} gr");
+Console.WriteLine($"Água {sal = (grama*2)/(1+0.7+0.4+0.02)/100} gr");
+
+
+
+
+
+        }
+        
+ static void MinhaAssinatura() 
+        {
+            LinhaAmarela();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Trabalho para Nota do Aluno ");
+            BarraAmarelaFinal();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Marcelo Camara Bertochi ");
+            BarraAmarelaFinal2();
+            BarraAmarelaInicio();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write(" Curso Informática I - Noite ");
+            BarraAmarelaFinal();
+            LinhaAmarela();
+        }
+
+        static void LinhaAmarela()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("-------------------------------\n");
+            Console.ResetColor();
+        }
+
+        static void BarraAmarelaInicio()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|");
+            Console.ResetColor();
+        }
+        static void BarraAmarelaFinal()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|\n");
+            Console.ResetColor();
+        }
+
+        static void BarraAmarelaFinal2()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("|\n".PadLeft(6, ' '));
+            Console.ResetColor();
+        }
+        static void ResetarCor()
+        {
+            Console.ResetColor();
+        }
+
+        static void LimparTela()
+        {
+            Console.Clear();
+        }
+
+        static void PularLinha()
+        {
+            Console.WriteLine();
+        }
+
+
+    }
+}
--- padaria/README.md
+++ padaria/README.md
@@ -1,2 +1,39 @@
 # exerciciosEtec2
 Exercicios da Etec
+
+Exercício FormulaDoPadeiro
+Em panificação, as receitas são geralmente formuladas em razão da quantidade de farinha (chamada de cem porcento).
+
+Considere, por exemplo, a seguinte receita de pão italiano:
+
+Ingrediente	Quantidade (em gramas)	Fórmula do Padeiro
+Farinha	500 g	100%
+Água	350 g	70%
+Fermento	200 g	40%
+Sal	10 g	2%
+Faça um programa que calcule a quantidade de ingredientes necessários para a confecção de um pão italiano (a partir da receita acima) para que o peso obtido ao final coincida com o peso desejado.
+
+Percentual do pão em relação ao peso da farinha = soma dos percentuais da receita (no exemplo, 1 + 0,7 + 0,4 + 0,02 = 2,12)
+Quantidade de farinha (o chamado cem porcento) = Peso desejado / Percentual do pão em relação ao peso da farinha
+Demais quantidades = Quantidade de farinha x Percentual do ingrediente na receita
+Exemplos:
+
+--- Padaria ---
+
+Peso desejado de pão italiano (em gramas)...: 5000
+
+
+Ingredientes:
+Farinha....: 2.358 g
+Água.......: 1.651 g
+Fermento...: 943 g
+Sal........: 47 g
+--- Padaria ---
+
+Peso desejado de pão italiano (em gramas)...: 1060
+
+Ingredientes:
+Farinha....: 500 g
+Água.......: 350 g
+Fermento...: 200 g
+Sal........: 10 g
